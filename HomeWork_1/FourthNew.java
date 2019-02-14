63faf9455618c40ca3403e4569ee72b1102b6f14
lumexx <pavel.chebonenko@gmail.com>
2019-02-15 00:18:12 +0300

    last upd

--- HomeWork_1/FourthNew.java
+++ HomeWork_1/FourthNew.java
@@ -4,38 +4,38 @@ import java.util.Scanner;
 
 public class FourthNew {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) 
+	{
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter N:");
         int n = scan.nextInt();
         int progressionCount = 0;
-        while (Math.abs(progressionCount) < 3)
-        {
-            if (n / 10 % 10 < n % 10 && progressionCount >= 0)
-            {
+        while (Math.abs(progressionCount) < 3) 
+		{
+            if ((n / 10 % 10 < n % 10) && progressionCount >= 0) 
+			{
                 progressionCount++;
-            }
-            else if (n / 10 % 10 > n % 10 && progressionCount <= 0)
-            {
+            } 
+			else if ((n / 10 % 10 > n % 10) && progressionCount <= 0) 
+			{
                 progressionCount--;
-            }
-            else
-                {
+            } 
+			else 
+			{
                 progressionCount = 4;
             }
             n /= 10;
         }
-        if (progressionCount == 3)
-        {
+        if (progressionCount == 3) 
+		{
             System.out.println("Increasing sequence");
-        }
-        else if (progressionCount == -3)
-        {
+        } 
+		else if (progressionCount == -3) 
+		{
             System.out.println("Decreasing sequence");
-        }
-        else
-        {
+        } 
+		else 
+		{
             System.out.println("No sequence");
         }
     }
